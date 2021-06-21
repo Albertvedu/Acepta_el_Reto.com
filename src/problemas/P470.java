2300f351c5de06731ff88a8b601810c90e639b12
Albertvedu <albert.vedu@gmail.com>
2021-06-21 21:52:55 +0200

    second problem

--- src/problemas/P470.java
+++ src/problemas/P470.java
@@ -12,23 +12,31 @@ public class P470 {
         while(counterLigth < 500000) {
             isValid = true;
             String cadena = sc.nextLine();
-            counterLigth += cadena.length();
-
-            for (int i = 0; i < cadena.length(); i++) {
-                if ((cadena.charAt(i) != 'R') && (cadena.charAt(i) != 'A') && (cadena.charAt(i) != 'V')) isValid = false;
-            }
+            isValid = isValidIntro(isValid, cadena);
             int counter = 0;
+            if ( counterLigth + cadena.length() > 500000) isValid = false;
+            if ( isValid )   counterLigth += cadena.length();
+            counter = getSearchLigth(isValid, cadena, counter);
 
-            while ( isValid && cadena.length() > 2 ) {
-                int pos = cadena.indexOf("RAV");
-                if (pos != -1) {
-                    String sSubCadena = cadena.substring(0, pos);
-                    String a = cadena.substring(pos + 3);
-                    counter++;
-                    cadena = sSubCadena + a;
-                } else break;
-            }
             if ( isValid ) System.out.println(counter);
         }
+    }
+    private static int getSearchLigth(boolean isValid, String cadena, int counter) {
+        while ( isValid && cadena.length() > 2 ) {
+            int pos = cadena.indexOf("RAV");
+            if (pos != -1) {
+                String sSubCadena = cadena.substring(0, pos);
+                String a = cadena.substring(pos + 3);
+                counter++;
+                cadena = sSubCadena + a;
+            } else break;
+        }
+        return counter;
+    }
+    private static boolean isValidIntro(boolean isValid, String cadena) {
+        for (int i = 0; i < cadena.length(); i++) {
+            if ((cadena.charAt(i) != 'R') && (cadena.charAt(i) != 'A') && (cadena.charAt(i) != 'V')) isValid = false;
+        }
+        return isValid;
     }
 }
